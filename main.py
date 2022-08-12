7656e503ea5bc168e2f8d66a4e5795e923250e28
longap <20084295@tafe.wa.edu.au>
2022-08-12 16:27:09 +0800

    Enishal Commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import random
+
+print(random.randint(0, 1))
+
+print ("choose heads or tails")
+
+
+choice=input("Enter your choice (heads or tails): ")
+
+num=random.randint(1,2)
+
+if num==1:
+    result="heads"
+
+elif num==2:
+    result="tails"
+
+if choice==result:
+    print("You Win",result)
+
+else:
+    print("You Loose")
+
+print("Thanks For Playing")
